a0811654dc17520553910fbf77f96ef983c53786
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2021-05-17 11:10:24 -0400

    added photo cropper

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Hello from './components/Hello/index'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import Hello from './components/Hello';
+import Photo from './components/Photos';
 import './App.global.css';
-
-
 
 export default function App() {
   return (
     <Router>
+      <nav>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+        <NavLink to="/photo" exact>
+          Photo
+        </NavLink>
+      </nav>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route exact path="/photo" component={Photo} />
+        <Route exact path="/" component={Hello} />
       </Switch>
     </Router>
   );
